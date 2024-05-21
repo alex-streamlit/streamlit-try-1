8f0e7ad263b7924690e491c7c44d64ff8a132aa0
alex-streamlit <alexandreveillerette@proton.me>
2024-05-21 20:22:52 +0100

    Update compositemodel1.py

--- compositemodel1.py
+++ compositemodel1.py
@@ -146,10 +146,10 @@ with tab3:
     st.write("This is the contact page.")
     
     def show_pdf(file_path):
-    with open(file_path, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
+        with open(file_path, "rb") as f:
+            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+        pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+        st.markdown(pdf_display, unsafe_allow_html=True)
 
     # Title of the app
     st.title("PDF Viewer")
