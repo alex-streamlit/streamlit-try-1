e5cbbc67805ced9893c3691bf52c48bfcea884d4
alex-streamlit <alexandreveillerette@proton.me>
2024-05-21 21:12:08 +0100

    Update compositemodel1.py

--- compositemodel1.py
+++ compositemodel1.py
@@ -1,11 +1,7 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-import streamlit.components.v1 as components
-import numpy as np
-import base64
+from pdf2image import convert_from_path
 import os
 from PIL import Image
-from pdf2image import convert_from_path
 
 def displayPDF(file):
     # Convert PDF to a list of image objects
@@ -29,11 +25,8 @@ with tab1:
 with tab2:
     st.title("About Page")
     st.write("This is the about page.")
-
-    st.header('Composite Model for $\\phi_{\\mathrm{op}}$')
-    st.subheader('Interactive Plot')
-
-    # Sidebar for parameter inputs
+    
+    # Render the sidebar only on the "About" page
     st.sidebar.header('System Parameters')
 
     st.markdown(r'Adjust system parameters on the left sidebar. May take a second to update.')
@@ -154,6 +147,7 @@ with tab2:
 
     components.html(f'<iframe src="https://www.desmos.com/calculator/worwfdu6lu?embed" width="680" height="400" style="border: 1px solid #ccc" frameborder=0></iframe>', width=680, height=500)
 
+# Content for Contact Tab
 with tab3:
     st.title("Contact Page")
     st.write("This is the contact page.")
