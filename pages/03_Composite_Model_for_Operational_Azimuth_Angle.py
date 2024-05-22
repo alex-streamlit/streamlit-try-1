c19358cdd60ca5d2f23453b9e2a384a3b07eb037
alex-streamlit <alexandreveillerette@proton.me>
2024-05-22 14:17:18 +0100

    Update 03_Composite_Model_for_Operational_Azimuth_Angle.py

--- pages/03_Composite_Model_for_Operational_Azimuth_Angle.py
+++ pages/03_Composite_Model_for_Operational_Azimuth_Angle.py
@@ -93,13 +93,6 @@ def plot_graph():
   
   # Adjust the legend
   plt.legend(loc='upper right', fontsize=12)
-    
-  current_dir = os.getcwd()
-  # Define the path to save the file
-  save_path = os.path.join(current_dir, 'high_quality_plot.png')
-  
-  # Save the plot with high resolution
-  plt.savefig(save_path, dpi=300)
   
   st.pyplot(plt)
 
