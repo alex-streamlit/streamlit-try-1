32a6fb3146e4e0c8826d0b362ddea931199cb9ea
alex-streamlit <alexandreveillerette@proton.me>
2024-05-25 18:00:43 +0100

    Update 02_Comparison_Wave_Equation.py

--- pages/02_Comparison_Wave_Equation.py
+++ pages/02_Comparison_Wave_Equation.py
@@ -1,2 +1,12 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import streamlit.components.v1 as components
+import numpy as np
+import base64
+from PIL import Image
+from pdf2image import convert_from_path
+import os
 
+
+st.set_page_config(page_title="Lost link! Give me a sec", page_icon="🌊", layout="wide")
 
